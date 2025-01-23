d139acf91af21c6e79e0ea3cda0d1ca7763a689b
Joshua Fourie <joshua@fourie.se>
2025-01-23 11:27:26 +0100

    Simple character with movements for moving back and forth.

--- common/main.c
+++ common/main.c
@@ -4,6 +4,19 @@
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 500
 
+typedef struct Player
+{
+    Vector2 position;
+    Vector2 velocity;
+    Vector2 size;
+} Player;
+
+Player player = {
+    .position = {300, 225},
+    .velocity = {50.0f, 0.0f},
+    .size = {50, 150},
+};
+
 int main()
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Online Fighting Game");
@@ -11,6 +24,16 @@ int main()
 
     while (!WindowShouldClose())
     {
+        if (IsKeyDown(KEY_D))
+        {
+            player.position.x += player.velocity.x * GetFrameTime();
+        }
+
+        if (IsKeyDown(KEY_A))
+        {
+            player.position.x -= player.velocity.x * GetFrameTime();
+        }
+
         if (IsKeyPressed(KEY_ENTER) && IsKeyDown(KEY_LEFT_ALT))
         {
             int display = GetCurrentMonitor();
@@ -28,7 +51,9 @@ int main()
         }
 
         BeginDrawing();
+        ClearBackground(BLACK);
         DrawRectangleV((Vector2){0, 375}, (Vector2){SCREEN_WIDTH, 125}, WHITE);
+        DrawRectangleV(player.position, player.size, RED);
         EndDrawing();
     }
     CloseWindow();
